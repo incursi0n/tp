9b738e93ef55a217b167e32365257c27ff439717
EOKwingster <y1709175257@outlook.com>
2026-03-19 15:07:44 +0800

    Add more TagType and remove String description

--- src/main/java/seedu/address/model/tag/TagType.java
+++ src/main/java/seedu/address/model/tag/TagType.java
@@ -4,16 +4,8 @@ package seedu.address.model.tag;
  * Represents types of Tag in the address book.
  */
 public enum TagType {
-    TAG("Tag"),;
-
-    private final String description;
-
-    TagType(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return this.description;
-    }
+    TAG,
+    COURSE,
+    TUTORIAL,
+    LAB
 }
